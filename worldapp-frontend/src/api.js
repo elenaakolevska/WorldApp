203a90bc206ae0dc0358e158018620777141c7e6
Elena <elena.kolevska@students.finki.ukim.mk>
2025-06-30 21:35:37 +0200

    Change in api.js4

--- worldapp-frontend/src/api.js
+++ worldapp-frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://worldapp-backend-latest.onrender.com",
+    baseURL: "https://worldapp-backend-latest.onrender.com/api",
   withCredentials: true,
 });
 
