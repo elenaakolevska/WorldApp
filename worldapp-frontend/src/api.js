465fb985af61a4337ba791fde32b76968a997ca5
Elena <elena.kolevska@students.finki.ukim.mk>
2025-06-30 21:52:21 +0200

    Change in api.js5

--- worldapp-frontend/src/api.js
+++ worldapp-frontend/src/api.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
+let baseURL = "";
+
+const hostname = window.location.hostname;
+
+if (hostname === "localhost") {
+  baseURL = "http://localhost:5000/api";
+} else if (hostname === "worldapp.local") {
+  baseURL = "/api";
+} else {
+  baseURL = "https://worldapp-backend-latest.onrender.com/api";
+}
+
 const API = axios.create({
-    baseURL: "https://worldapp-backend-latest.onrender.com/api",
+  baseURL,
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
